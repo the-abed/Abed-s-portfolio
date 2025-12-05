6ba11e328267f6389f042e0ece734e2d1cd34c06
rashel9255 <ibrahim2000167@gmail.com>
2025-12-06 01:34:45 +0600

    portfolio updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,34 @@
-import { StarBackground } from '@/components/StarBackground';
-import { AboutSection } from '../components/AboutSection';
-import { ContactSection } from '../components/ContactSection';
-import { Footer } from '../components/Footer';
-import { HeroSection } from '../components/HeroSection';
-import { Navbar } from '../Components/Navbar';
-import { ProjectsSection } from '../components/ProjectsSection';
-import { SkillsSection } from '../components/SkillsSection';
-import { TestimonialSection } from '../components/Testimonial';
+import { StarBackground } from "@/Components/StarBackground";
+import { AboutSection } from "../Components/AboutSection";
+import { ContactSection } from "../Components/ContactSection";
+import { Footer } from "../Components/Footer";
+import { HeroSection } from "../Components/HeroSection";
+import { Navbar } from "../Components/Navbar";
+import { ProjectsSection } from "../Components/ProjectsSection";
+import { SkillsSection } from "../Components/SkillsSection";
+import { TestimonialSection } from "../Components/Testimonial";
 
 export const Home = () => {
-  return (
-    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Theme Toggle */}
-      {/* Background Effects */}
-      <StarBackground />
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme Toggle */}
+            {/* Background Effects */}
+            <StarBackground />
 
-      {/* Navbar */}
-      <Navbar />
-      {/* Main Content */}
-      <main>
-        <HeroSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <TestimonialSection />
-        <ContactSection />
-      </main>
+            {/* Navbar */}
+            <Navbar />
+            {/* Main Content */}
+            <main>
+                <HeroSection />
+                <AboutSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <TestimonialSection />
+                <ContactSection />
+            </main>
 
-      {/* Footer */}
-      <Footer />
-    </div>
-  );
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
 };
